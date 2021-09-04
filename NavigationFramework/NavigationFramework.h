ec43e5194a14d37d2606d623b2572f1fca72960a
Zlobrynya <zlobrynya@gmail.com>
2021-09-04 13:48:31 +0300

    Initial Commit

---
+++ NavigationFramework/NavigationFramework.h
@@ -0,0 +1,18 @@
+//
+//  NavigationFramework.h
+//  NavigationFramework
+//
+//  Created by Nikita Nikitin on 04.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NavigationFramework.
+FOUNDATION_EXPORT double NavigationFrameworkVersionNumber;
+
+//! Project version string for NavigationFramework.
+FOUNDATION_EXPORT const unsigned char NavigationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationFramework/PublicHeader.h>
+
+
